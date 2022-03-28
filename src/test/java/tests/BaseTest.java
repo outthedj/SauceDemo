368b338f58333a55a606dc57abcad0433c462d9d
igorv <igorvolynets1985@gmail.com>
2022-03-29 01:26:31 +0300

    Final of 14 Homework

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,20 +4,27 @@ import driver.factorydriver.DriverFactory;
 import driver.factorydriver.DriverManager;
 import driver.factorydriver.DriverType;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 import staticdata.WebTimeouts;
+import utilities.TestListeners;
+
 import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
+@Listeners(TestListeners.class)
 public class BaseTest {
 
     WebDriver driver;
     DriverManager driverManager;
 
-    @BeforeMethod
-    public void setUp() throws MalformedURLException {
+    @BeforeSuite
+    @Parameters({"browser"})
+    public void setUp(@Optional("chrome") String browser) throws MalformedURLException {
         DriverFactory factory = new DriverFactory();
+        DriverType driverType = null;
+        if(browser.equals("chrome")) {
+            driverType = DriverType.CHROME;
+        }
         driverManager = factory.getManager(DriverType.REMOTE);
         driverManager.createDriver();
         driver = driverManager.getDriver();
@@ -27,7 +34,11 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(WebTimeouts.IMPLICIT_TIMEOUT, TimeUnit.SECONDS);
     }
 
-    @AfterMethod
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @AfterSuite
     public void tearDown() {
         driver.quit();
     }
